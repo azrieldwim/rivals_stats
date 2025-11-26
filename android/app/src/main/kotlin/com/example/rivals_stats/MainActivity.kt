f24285dfa23e98742a6e8e1530b3a58ef3876d34
Azriel Dwi Mahendra <azrieldwim@gmail.com>
2025-11-26 07:59:23 +0700

    chore: initialize rivals stats project

---
+++ android/app/src/main/kotlin/com/example/rivals_stats/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rivals_stats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:rivals_stats/config/routes/app_pages.dart';
+import 'package:rivals_stats/config/routes/app_routes.dart';
+import 'package:rivals_stats/config/theme/app_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
+      title: 'Rivals Stats',
+      initialRoute: Routes.main,
+      getPages: AppPages.pages,
+    );
+  }
+}
