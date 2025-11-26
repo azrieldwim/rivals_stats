19c9df98325d94e201a392181f9dbacb685c9a61
Azriel Dwi Mahendra <azrieldwim@gmail.com>
2025-11-26 08:01:49 +0700

    style: setup theme and colors

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0e0d1a);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gold = Color(0xFFdbc368);
+  static const Color primary = Color(0xFFfdd92d);
+
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFFa4a2b8);
+
+  static const Color red = Color(0x00FF4545);
+  static const Color cardBackground = Color(0xFF1c1b2e);
+  static const Color cardBackgroundPurple = Color(0xFF2f3146);
+}
